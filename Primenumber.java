1adb3dcac779e0d02fb109cad8ce355676167b0b
Nivedita53 <112791773+Nivedita53@users.noreply.github.com>
2022-09-04 11:06:45 +0530

    Add files via upload

---
+++ Primenumber.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Primenumber {
+
+	public static void main(String[] args) {
+		int n=6;
+		for(int i=2;i<=n-1;i++) {
+			if(n%i == 0) {
+				System.out.println("This number is prime");
+			}
+				else {
+					System.out.println("this number is not prime");
+				
+			}
+			
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
